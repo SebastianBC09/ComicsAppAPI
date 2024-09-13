0779eecfcc35df8e87f80997704ad485ad04299e
Sebastian Ballen <sebastian.ballenc@gmail.com>
2024-09-12 21:37:00 -0500

    Feat: migrations made & db updated

---
+++ Migrations/20240913020347_UpdateProductoImageUrl.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ComicsAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateProductoImageUrl : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Imagen",
+                table: "Productos",
+                newName: "ImageURL");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "ImageURL",
+                table: "Productos",
+                newName: "Imagen");
+        }
+    }
+}
