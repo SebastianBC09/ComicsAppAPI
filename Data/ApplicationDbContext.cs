24a41f6d6e36ffc2904193cfb5a5374b30046d33
Sebastian Ballen <sebastian.ballenc@gmail.com>
2024-09-12 22:37:23 -0500

    Feat: db context file updated for new entities

--- Data/ApplicationDbContext.cs
+++ Data/ApplicationDbContext.cs
@@ -4,5 +4,7 @@ namespace ComicsAPI.Data {
     public class ApplicationDbContext : DbContext {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options): base (options) {}
         public DbSet<Producto> Productos { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+        public DbSet<ProductoDeseado> ProductosDeseados { get; set; }
     }
 }
