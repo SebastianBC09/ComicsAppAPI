80891eb62464b06fe9f8489e1424a592865a44fb
Sebastian Ballen <sebastian.ballenc@gmail.com>
2024-09-12 22:57:54 -0500

    Feat: Swagger added to project

--- Program.cs
+++ Program.cs
@@ -3,24 +3,35 @@ using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
+// Swagger configuration for API documentation
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+// CORS policy to allow all origins, methods and headers
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll",
-        builder =>
-        {
-            builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
-        });
+  options.AddPolicy("AllowAll",
+      builder =>
+      {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+      });
 });
-
+// Database configuration with SQLite and Entity Framework Core
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
-
+// Registering the ProductosController
 builder.Services.AddControllers();
 var app = builder.Build();
+// Swagger configuration for API documentation
+if (app.Environment.IsDevelopment())
+{
+  app.UseSwagger();
+  app.UseSwaggerUI();
+}
 
 app.UseCors("AllowAll");
 app.UseHttpsRedirection();
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
