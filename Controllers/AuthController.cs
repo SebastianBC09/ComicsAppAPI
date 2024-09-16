7d9f53f557ee7631f4c52782e6e361b546b09a1a
Sebastian Ballen <sebastian.ballenc@gmail.com>
2024-09-15 20:09:20 -0500

    Feat: Auth Controller created

---
+++ Controllers/AuthController.cs
@@ -0,0 +1,58 @@
+using ComicsAPI.Data;
+using ComicsAPI.Models;
+using ComicsAPI.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace ComicsAPI.Controllers
+{
+  [Route("api/auth")]
+  [ApiController]
+  public class AuthController : ControllerBase
+  {
+    private readonly ApplicationDbContext _context;
+    private readonly PasswordService _passwordService;
+
+    public AuthController(ApplicationDbContext context, PasswordService passwordService)
+    {
+      _context = context;
+      _passwordService = passwordService;
+    }
+
+    // REGISTRO
+    [HttpPost("registro")]
+    public async Task<IActionResult> Register([FromBody] Usuario usuario)
+    {
+      if (await _context.Usuarios.AnyAsync(u => u.Email == usuario.Email))
+      {
+        return BadRequest("El email ya está registrado");
+      }
+
+      usuario.PasswordHash = _passwordService.HashPassword((usuario.PasswordHash));
+
+      _context.Usuarios.Add(usuario);
+      await _context.SaveChangesAsync();
+
+      return Ok(usuario);
+    }
+
+    // LOGIN
+    [HttpPost("login")]
+    public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
+    {
+      var usuario = await _context.Usuarios.FirstOrDefaultAsync(u => u.Email == loginRequest.Email);
+      if (usuario == null)
+      {
+        return Unauthorized("El correo electronico o la contrasena son incorrectos");
+      }
+
+      var isPasswordValid = _passwordService.VerifyPassword(usuario.PasswordHash, loginRequest.Password);
+      if (!isPasswordValid)
+      {
+        return Unauthorized("El correo electronico o la contrasena son incorrectos");
+      }
+
+      return Ok("Inicio de sesion exitoso!");
+    }
+  }
+}
